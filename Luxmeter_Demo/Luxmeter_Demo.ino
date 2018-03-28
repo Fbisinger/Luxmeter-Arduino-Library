b78b0b03c2f011eb2045e28802303598347cd956
Fbisinger <37881821+Fbisinger@users.noreply.github.com>
2018-03-28 21:53:07 +0200

    Add files via upload

---
+++ Luxmeter_Demo/Luxmeter_Demo.ino
@@ -0,0 +1,21 @@
+/* ============================================
+Arduino Luxmeter - Sensor Bibliothek
+
+------------2018 F.Bisinger--------------------
+
+Diese Bibliothek konvertiert die Analogen 
+Eingangswerte eines LDR in Lux - Werte
+
+===============================================
+*/
+#include "Luxmeter.h"
+Luxmeter Demo_Sensor;
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  Serial.print(Demo_Sensor.getLux(3)); /* LDR im Spannungsteiler (5K Ohm Widerstand) an Analog Pin 3 WICHTIG!! GND auf der Seite des 5K Ohm Widerstandes und 5V auf der Seite des LDR anschließen*/
+  Serial.println(" lux");
+  delay(100);
+}
