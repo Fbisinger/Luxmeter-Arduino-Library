0296c17f6033447535262b98f16f925ae3b9b8c1
Fbisinger <37881821+Fbisinger@users.noreply.github.com>
2018-03-28 21:43:06 +0200

    Add files via upload

---
+++ Luxmeter.cpp
@@ -0,0 +1,52 @@
+/* ============================================
+Arduino Luxmeter - Sensor Bibliothek
+Copyright (c) 2018 F.Bisinger
+
+ Diese Bibliothek konvertiert die Analogen Eingangswerte eines LDR in Lux - Werte
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+===============================================
+*/
+#include "Luxmeter.h"
+
+Luxmeter::Luxmeter() { 
+}
+
+int Luxmeter::getLux(int ldr) {
+  _ldr = ldr;
+  pinMode(_ldr,INPUT);
+  #define MAX_ADC_READING           1023
+  #define ADC_REF_VOLTAGE           5.0
+  #define REF_RESISTANCE            5030
+  #define LUX_CALC_SCALAR           12518931
+  #define LUX_CALC_EXPONENT         -1.405
+  int   ldrRawData;
+  float resistorVoltage, ldrVoltage;
+  float ldrResistance;
+  float ldrLux;
+  ldrRawData = analogRead(_ldr);
+  resistorVoltage = (float)ldrRawData / MAX_ADC_READING * ADC_REF_VOLTAGE;
+  ldrVoltage = ADC_REF_VOLTAGE - resistorVoltage;
+  ldrResistance = ldrVoltage/resistorVoltage * REF_RESISTANCE;
+  ldrLux = LUX_CALC_SCALAR * pow(ldrResistance, LUX_CALC_EXPONENT);
+
+ return ldrLux;
+}
+
+
